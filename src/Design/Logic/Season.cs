0ce888285a074011da6bad2fab429967cb2b9561
Mike <mikeundead@googlemail.com>
2018-11-06 19:47:34 +0100

    interface

--- src/Design/Logic/Season.cs
+++ src/Design/Logic/Season.cs
@@ -4,5 +4,20 @@ namespace Design.Logic
 {
     public class Season : ISeason
     {
+        public string Image => throw new System.NotImplementedException();
+
+        public string Name => throw new System.NotImplementedException();
+
+        public string EpisodeText => throw new System.NotImplementedException();
+
+        public string Type => throw new System.NotImplementedException();
+
+        public string Status => throw new System.NotImplementedException();
+
+        public string Score => throw new System.NotImplementedException();
+
+        public string Aired => throw new System.NotImplementedException();
+
+        public string Rating => throw new System.NotImplementedException();
     }
 }
