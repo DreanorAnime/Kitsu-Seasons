eb1c872d8d14c3533d14016dc59764d23f9bbb2d
Mike <mikeundead@googlemail.com>
2018-11-07 19:27:36 +0100

    last cleanup

--- prebuild.ps1
+++ prebuild.ps1
@@ -1,5 +1,5 @@
 git submodule update --init --recursive --remote #--init is a workaround for appveyor
 
-nuget restore src\submodules\Kitsu\Kitsu.sln
-nuget restore src\submodules\Library\Library.sln
+nuget restore src\submodules\Kitsu\src\Kitsu.sln
+nuget restore src\submodules\Library\src\Library.sln
 nuget restore src\kitsuseasons.sln
